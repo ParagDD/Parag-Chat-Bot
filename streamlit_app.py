6a263bca97bd45c1c9534b9e1cb7297cc5cace3e
ParagDD <105158139+ParagDD@users.noreply.github.com>
2023-10-07 13:27:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.markdown(
     """
     <style>
     /* Hide the GitHub logo */
-    .stActionButton {
+    .st-emotion-cache-1p1m4ay.e3g6aar0 {
         display: none !important;
     }
     </style>
